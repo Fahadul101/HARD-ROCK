1f7c24d26b6752fc0a63dee769044f71fcadfc01
Fahadul Islam <fahadulislam112@gmail.com>
2021-02-09 12:04:18 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,55 @@
+const searchSongs = () =>
+{
+    const searchText = document.getElementById('search-field').value;
+    // console.log(searchText);
+    const url = `https://api.lyrics.ovh/suggest/${searchText}`
+    // console.log(url);
+    //load data
+    fetch(url)
+    .then(res => res.json())
+     //We see that its object but we need to show array.
+    .then(data => displaySongs(data.data))
+}
+
+const displaySongs = songs =>
+{    
+
+    const songContainer =document.getElementById('song-container');
+    songContainer.innerHTML = '';
+
+    // console.log(songs);
+   songs.forEach(song =>{
+       const songDiv = document.createElement('div');
+       songDiv.className = 'single-result row align-items-center my-3 p-3' ;
+       songDiv.innerHTML = `
+                                <div class="col-md-9">
+                                     <h3 class="lyrics-name">${song.title}</h3>
+                                     <p class="author lead">Album by <span>${song.artist.name}</span></p>
+                                     <audio controls>
+                                     <source src="${song.preview}"  type ="audio/mpeg">
+                                     </audio>
+                                </div>
+                                <div  class="col-md-3 text-md-right text-center">
+                                    <button onclick = "getLyric('${song.artist.name}','${song.title}')" class="btn btn-success">Get Lyrics</button>
+                                </div>
+       `;
+       
+       songContainer.appendChild(songDiv);
+
+   })
+
+   
+}
+const getLyric = (artist,title) =>
+   {
+       const url = `https://api.lyrics.ovh/v1/${artist}/${title}`;
+       
+       fetch(url)
+       .then(res => res.json())
+       .then(data => displayLyrics(data.lyrics))
+   }
+   const displayLyrics = lyrics =>
+   {
+       const lyricsDiv = document.getElementById('song-lyrics')
+       lyricsDiv.innerText = lyrics;
+   }
